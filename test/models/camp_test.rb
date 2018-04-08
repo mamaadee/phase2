0f79f9c1efba56bf588bb2d7d241993f89ba901a
Maryam <mamaadee@andrew.cmu.edu>
2018-04-08 09:23:48 +0000

    Working on the curriculum

--- test/models/camp_test.rb
+++ test/models/camp_test.rb
@@ -32,12 +32,12 @@ class CampTest < ActiveSupport::TestCase
 
     should "Have a scope to select only active Camps" do
       assert_equal 5, Camp.active.size
-      assert_equal ["C1","C3", "C4","C5", "C6"], Camp.active.all.map{|c| c.curriculum.name}
+      assert_equal ["C1","C3", "C4","C5", "C6"], Camp.active.all.map{|c| c.curriculum.name}.sort
     end
     
     should "Have a scope to select only inactive Camps" do
       assert_equal 1, Camp.active.size
-      assert_equal ["C2"], Camp.active.all.map{|c| c.curriculum.name}
+      assert_equal ["C2"], Camp.inactive.all.map{|c| c.curriculum.name}.sort
     end
       
     should "have a scope to order alphabetically by camp name" do
@@ -45,15 +45,8 @@ class CampTest < ActiveSupport::TestCase
     end
 
     should "have a scope to order chronologically by due_on date" do
-      assert_equal ["C1","C2","C3", "C4","C5", "C6","C7"], Camp.chronologicalalphabetical.map{|c| c.curriculum.name}
+      assert_equal ["C1","C2","C3", "C4","C5", "C6","C7"], Camp.chronological.all.map{|c| "#{c.name}-#{c.start_date.strftime("%b %d")}"}
     end
 
-    should "have a scope for upcoming Camps" do
-      assert_equal 6, Camp.upcoming.size
-    end
-
-    should "have a scope for past Camps" do
-      assert_equal 1, Camp.past.size
-    end
   end
 end
--- test/sets/curriculum_contexts.rb
+++ test/sets/curriculum_contexts.rb
@@ -1,14 +1,29 @@
-module CurriculumContexts
-  def create_curriculums
-    @a = FactoryBot.create(:curriculum)
-    @b = FactoryBot.create(:curriculum, name: "B", active: false)
-    @c = FactoryBot.create(:curriculum, name: "C")
+module Contexts
+  module CurriculumContexts
+    def create_curriculums
+      @calc    = FactoryBot.create(:curriculum)
+      @java   = FactoryBot.create(:curriculum, name: "Java", min_rating: 450, max_rating: 800)
+      @ruby = FactoryBot.create(:curriculum, name: "Ruby", active: false)
+    end
+
+    def destroy_curriculums
+      @calc.destroy
+      @java.destroy
+      @ruby.destroy
+    end
+
+    def create_more_curriculums
+      @python = FactoryBot.create(:curriculum, name: "Python", min_rating: 500, max_rating: 100, description: "About python", active: false)
+      @english = FactoryBot.create(:curriculum, name: "English", min_rating: 1000, max_rating: 3000, description: "Learn English.")
+      @chess = FactoryBot.create(:curriculum, name: "Chess", min_rating: 0, max_rating: 500, description: "Learn the tactics of chess")
+      @sport = FactoryBot.create(:curriculum, name: "Sport", min_rating: 1000, max_rating: 1800, description: "Sports")
+    end
+
+    def destroy_more_curriculums
+      @python.destroy
+      @english.destroy
+      @chess.destroy
+      @sport.destroy
+    end
   end
-  
-  def destroy_curriculums
-    @a.destroy
-    @b.destroy
-    @c.destroy
-  end
-  
 end
